14b8f955803c921a57cc534a29a4fb19eb9398b2
Hari Prakash <haritvs@live.in>
2019-09-27 16:38:46 +0530

    Added Register functionality

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -20,6 +21,42 @@ namespace WishList.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register", model);
+            }
+
+            var user = new ApplicationUser();
+            user.Email = model.Email;
+            user.UserName = model.Email;
+            string password = model.Password;
+
+            var result = _userManager.CreateAsync(user, password);
+            if (result.Result.Succeeded == false)
+            {
+                foreach (var err in result.Result.Errors)
+                {
+                    ModelState.AddModelError("Password", err.Description);
+                }
+
+                return View("Register", model);
+            }
+
+
+            return RedirectToAction("HomeController.Index");
         }
     }
 }
